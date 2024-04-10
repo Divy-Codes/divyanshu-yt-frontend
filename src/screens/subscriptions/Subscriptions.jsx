7613cd8f9365b945182cdd13214b08253d18ad26
Divy-Codes <divyanshucodes@outlook.com>
2024-04-10 13:04:26 +0530

    Added skeletons for subscribed channels screen

--- src/screens/subscriptions/Subscriptions.jsx
+++ src/screens/subscriptions/Subscriptions.jsx
@@ -1,9 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
-import "./_subscriptions.scss";
-import { useEffect } from "react";
-import { getSubscriptions } from "../../redux/slices/subscriptionsSlice";
-import { Container } from "react-bootstrap";
-import ChannelRow from "../../components/otherSubComponents/ChannelRow";
+import { useDispatch, useSelector } from 'react-redux';
+import './_subscriptions.scss';
+import { useEffect } from 'react';
+import { getSubscriptions } from '../../redux/slices/subscriptionsSlice';
+import { Container } from 'react-bootstrap';
+import ChannelRow from '../../components/otherSubComponents/ChannelRow';
+import SubscriptionsSkeleton from '../../skeletons/SubscriptionsSkeleton';
 
 export default function Subscriptions() {
   const dispatch = useDispatch();
@@ -13,14 +14,17 @@ export default function Subscriptions() {
   const { loading, videos } = useSelector((state) => state.subscriptions);
   return (
     <Container>
-      {!loading ? (
-        videos &&
-        videos.map((video) => (
-          <ChannelRow key={video.id} video={video} subscriptions />
-        ))
-      ) : (
-        <h2>Loading...</h2>
-      )}
+      {!loading
+        ? videos &&
+          videos.map((video) => (
+            <ChannelRow key={video.id} video={video} subscriptions />
+          ))
+        : // <h2>Loading...</h2>
+          [...Array(20)].map((item, i) => (
+            // <Col md={4} key={i}>
+            <SubscriptionsSkeleton key={i} />
+            // </Col>
+          ))}
     </Container>
   );
 }
