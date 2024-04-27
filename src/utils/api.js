1427fe9ef4e0d5f8bbb77a57be7d3ea2e8e7623c
Divy-Codes <divyanshucodes@outlook.com>
2024-04-27 12:05:50 +0530

    final changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,12 +3,7 @@ import axios from 'axios';
 const request = axios.create({
   baseURL: 'https://youtube.googleapis.com/youtube/v3',
   params: {
-    // key: process.env.REACT_APP_API_KEY,
-    //main key
-    key: 'AIzaSyBD4CyQHEfDBKeTp_O8LQ1V8hqhIt0-ywI',
-
-    //test-key
-    // key: 'AIzaSyD5XCPsRglqzBJhlPLpAMqtfvJVxt7iceE',
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
