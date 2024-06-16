19d22aaac5495857fc23d0ced812ec4e3ccd3ff4
Divy-Codes <147299807+Divy-Codes@users.noreply.github.com>
2024-06-16 21:05:23 +0530

    api changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3",
   params: {
     // key: process.env.REACT_APP_API_KEY,
-    key: "AIzaSyBD4CyQHEfDBKeTp_O8LQ1V8hqhIt0-ywI",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
